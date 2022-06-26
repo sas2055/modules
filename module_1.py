7aaeb573fd2ae02c83bf41c6bd39cbd6c34a5a7d
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-06-26 22:43:02 +0530

    module_1.py

---
+++ module_1.py
@@ -0,0 +1,45 @@
+"""
+# Modules:
+       - any python file is a module
+       - it is a collection of different members, It has variables, class, function, method
+       and so many things Whatever is there in python, can be added into a module.
+------------------------------------------------------------------
+
+# we have 2 types:
+1. Built-in modules
+2. User defined module
+-------------------------------------------------------------
+
+1. Built-in modules:
+        present in python itself.
+ex - math, cvs, sys, os, array, itertools, collection, statistics
+-----------------------------------------------------------------
+
+# Que.1 how to check built-in modules
+help('modules')
+----------------------------------------------------------------
+
+# Que.2 if u want to check total python
+import builtins
+print(builtins)
+help('builtins')
+---------------------------------------------------------------
+
+# Que.3 built in module math
+import math
+print(math.e)    # 2.718281828459045
+print(math.pi)   # 3.141592653589793
+print(math.factorial(7))  # 5040
+print(math.log10(23))  # 1.3617278360175928
+print(math.log(6))    # 1.791759469228055
+----------------------------------------------------------
+
+# Que.4 built-in module statistics
+import statistics
+print(statistics.mean([1, 4, 3, 5, 6, 2]))   # mean is 3.5
+print(statistics.mode(['a', 's', 'a', 'a', 'w', 'a']))   # mode of a
+print(statistics.median([2, 3, 4, 6, 9, 1 ,7]))       # median is 4
+print(statistics.multimode(['a', 's', 'a', 'a', 'w', 'a', 'r', 'k']))  # multimode ['a']
+=============================================================================
+
+"""
