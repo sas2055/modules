82c81cf94ff3d48faf7c1c0e067bb1f6dd0c6425
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-07-08 18:18:48 +0530

    module_3.py

---
+++ module_3.py
@@ -0,0 +1,19 @@
+"""
+from info import name
+print(name)
+---------------------------------------------------------------
+
+# import info
+from info import *
+print(dir())
+===============================================================
+
+__pycache__:
+        when we import any module from a particular directory,
+    so its .pyc file gets created and stored in this folder.
+----------------------------------------------------------------
+
+# Que. why .pyc get created
+    to convert .py file to bytecode / machine code
+--------------------------------------------------------------------
+"""
